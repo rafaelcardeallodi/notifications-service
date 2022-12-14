f619dcd154efbc1563ba10a4f7ef9ffb832ee6ed
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-12-14 18:34:00 -0300

    Domínio, casos de uso e regras de negócio

--- src/infra/http/dtos/create-notification-body.ts
+++ src/infra/http/dtos/create-notification-body.ts
@@ -1,5 +1,4 @@
 import { IsNotEmpty, IsUUID, Length } from 'class-validator';
-import { isNotEmpty } from 'class-validator/types/decorator/decorators';
 
 export class CreateNotificationBody {
   @IsNotEmpty()
